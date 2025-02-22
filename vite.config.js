8b11a36d8e8de5c96606fc741d14feec37569a84
unknown <anshupal667@gmail.com>
2025-02-22 21:24:32 +0530

    ramayan latest code

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-﻿import { defineConfig } from 'vite'
+﻿
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ramayandeployed/", // 🔥 Add your repository name here
   server: {
     host: true,
+    // port: 3000,
     strictPort: true,
-  },
+  }
 })
+
